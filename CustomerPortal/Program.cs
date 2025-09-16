53950fc7ac8e31e15f4c7cd74a64b0b89427b639
Samitha Gunarathna <samitha@Samithas-MacBook-Air.local>
2025-09-16 12:53:52 +1000

    basic login view

--- CustomerPortal/Program.cs
+++ CustomerPortal/Program.cs
@@ -25,8 +25,12 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    // name: "default",
+    // pattern: "{controller=Home}/{action=Index}/{id?}")
+    // .WithStaticAssets();
+
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Login}/{action=Index}")
     .WithStaticAssets();
 
 
