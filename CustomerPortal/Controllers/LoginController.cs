c91f32ffcfd6b8a0366b0bbca4b08b022040d290
Samitha Gunarathna <samitha@Samithas-MacBook-Air.local>
2025-09-18 22:22:17 +1000

    login controller basic with obj remove from git

--- CustomerPortal/Controllers/LoginController.cs
+++ CustomerPortal/Controllers/LoginController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-
 namespace CustomerPortal.Controllers;
 
 public class LoginController : Controller
@@ -7,5 +6,21 @@ public class LoginController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult HandleLogin(string loginId, string password)
+    { 
+        // check inputs
+        if (string.IsNullOrEmpty(loginId) || string.IsNullOrEmpty(password))
+        {
+            // show error and return
+            return View("Index");
+        }
+        
+       // get login details
+       // verify the hash
+       // perform login
+       return RedirectToAction("Index", "Home");
     }
 }
