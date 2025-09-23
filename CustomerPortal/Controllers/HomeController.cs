cf6c181e4f025a544c7b6e2f2b55db7273edbee4
Samitha Gunarathna <samitha@Samithas-MacBook-Air.local>
2025-09-23 11:31:30 +1000

    reading accounts

--- CustomerPortal/Controllers/HomeController.cs
+++ CustomerPortal/Controllers/HomeController.cs
@@ -1,21 +1,37 @@
 using System.Diagnostics;
+using CustomerPortal.Data;
 using Microsoft.AspNetCore.Mvc;
 using CustomerPortal.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomerPortal.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly McbaContext mcbacontext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, McbaContext context)
     {
         _logger = logger;
+        mcbacontext = mcbacontext;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+      //  return View();
+      
+      // var accounts =  await mcbacontext.Accounts
+      //     .Where(a => a.CustomerID == 2100)
+      //     .ToListAsync();
+      
+      var accounts = new List<Account>
+      {
+          new Account { AccountNumber = 12345678, AccountType = AccountType.S, Balance = 2500.75m },
+          new Account { AccountNumber = 87654321, AccountType = AccountType.S, Balance = 1250.00m }
+      };
+
+      return View(accounts);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
